7fca36768bc5a0d0e0ca83d55e47fcaa5f7b2fb0
Samir Babacic <s.babacic@net2.one>
2023-03-21 09:22:20 +0100

    Add project files.

---
+++ MCA/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCA
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public bool Domestic { get; set; }
+        public float Price { get; set; }
+        public int? Weight { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MCA/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MCA
+{
+    internal class Program
+    {
+        private static string FormatPrice(float price) => "$" + price.ToString("0.0", System.Globalization.CultureInfo.GetCultureInfo("fr-FR"));
+
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://interview-task-api.mca.dev/")
+            };
+
+            var products = (await httpClient.
+                GetFromJsonAsync<List<Product>>($"qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1"));
+
+            if (products == null || !products.Any())
+            {
+                return;
+            }
+
+            products = products.OrderBy(x => x.Name).ToList();
+
+            var productDictionary = products.GroupBy(x => x.Domestic)
+                .ToDictionary(x => x.Key, x =>
+                new
+                {
+                    Products = x.OrderBy(x => x.Name).ToList(),
+                    Cost = x.Sum(x => x.Price)
+                });
+
+            var domesticProductGroup = productDictionary[true];
+            var importedProductGroup = productDictionary[false];
+
+            Console.WriteLine(".Domestic");
+            PrintProducts(domesticProductGroup.Products);
+            Console.WriteLine(".Imported");
+            PrintProducts(importedProductGroup.Products);
+
+            Console.WriteLine($"Domestic cost: {FormatPrice(domesticProductGroup.Cost)}");
+            Console.WriteLine($"Imported cost: {FormatPrice(importedProductGroup.Cost)}");
+
+            Console.WriteLine($"Domestic count: {domesticProductGroup.Products.Count}");
+            Console.WriteLine($"Imported count: {importedProductGroup.Products.Count}");
+        }
+
+        private static void PrintProducts(List<Product> products)
+        {
+            string spaceSeparator = "   ";
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"...{product.Name}");
+
+                Console.WriteLine($"{spaceSeparator}{FormatPrice(product.Price)}");
+
+                var descriptionToPrint = product.Description.Length < 10 ? product.Description : product.Description.Substring(0, 10) + "...";
+                Console.WriteLine($"{spaceSeparator}{descriptionToPrint}");
+
+                var weightToPrint = product.Weight.HasValue ? product.Weight.Value.ToString() : "N/A";
+                Console.WriteLine($"{spaceSeparator}{weightToPrint}");
+            }
+        }
+    }
+}
